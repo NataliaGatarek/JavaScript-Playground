3ea546ec24a06062cade0b9062e378233e4568d3
Nataliag <nova.nathalia@gmail.com>
2021-03-26 09:56:29 +0100

    1st JS playground

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Starting javascript");
+var MyName = "Natalia";
+console.log("Natalia");
+var Age = 40;
+console.log(33);
+var juliaAge = 32;
+var ageDiff = function age () {return(Age - juliaAge); }
+console.log(ageDiff());
+if (Age > 21) {
+    console.log("you are older than 21");
+}
+if (Age < 21) {
+    console.log("you are younger than 21");
+}
+ 
