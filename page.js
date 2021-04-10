17bfdd8a4eb19c19577a9021a4c791aede6aa966
NataliaGatarek <nova.nathalia@gmail.com>
2021-04-10 18:16:58 +0200

    new commit

--- page.js
+++ page.js
@@ -2,6 +2,19 @@
 var cols = 3;
 var rows = 2;
 var table = '';
+
+function newTable (cols, rows) {
+for (var r = 0; r < rows; r++) {
+  table += "<tr>";
+  for (var c = 0; c < cols; c++){
+    table +="<td>" +c + "</td>"
+  }
+  table += "</tr>";
+}
+  document.write("<table border ='1'>" + table + "</table>")
+}
+console.log(newTable(cols, rows));
+
 for (var r = 0; r < rows; r++) {
   table += "<tr>";
   for (var c = 0; c < cols; c++){
